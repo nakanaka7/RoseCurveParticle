3b32e3beb660c1a06b7f53ba9f1d884a610111a3
nakanakagames <nakanakagames@gmail.com>
2021-08-03 13:29:52 +0900

    delegate to CommandHandler

--- src/main/java/tokyo/nakanaka/roseCurve/Main.java
+++ src/main/java/tokyo/nakanaka/roseCurve/Main.java
@@ -1,17 +1,28 @@
 package tokyo.nakanaka.roseCurve;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import tokyo.nakanaka.CommandHandler;
 import tokyo.nakanaka.commandSender.CommandSender;
 
 public class Main {
+	private Map<String, CommandHandler> cmdHandlerMap = new HashMap<>();
 	
 	public void onRcpCommand(CommandSender cmdSender, String label, String[] args) {
-		System.out.println("hello");
+		CommandHandler cmdHandler = this.cmdHandlerMap.get(label);
+		if(cmdHandler != null) {
+			cmdHandler.onCommand(cmdSender, args);
+		}
 	}
 	
 	public List<String> onRcpTabComplete(CommandSender cmdSender, String label, String[] args) {
+		CommandHandler cmdHandler = this.cmdHandlerMap.get(label);
+		if(cmdHandler != null) {
+			return cmdHandler.onTabComplete(cmdSender, args);
+		}
 		return new ArrayList<>();
 	}
 	
