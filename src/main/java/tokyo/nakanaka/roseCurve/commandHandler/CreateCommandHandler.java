3359ad1dfa54d6668ab8094b200f5f536d81d84d
nakanakagames <nakanakagames@gmail.com>
2021-08-04 12:11:46 +0900

    rewrite/ add message/ add comment/ use CommandHelp

--- src/main/java/tokyo/nakanaka/roseCurve/commandHandler/CreateCommandHandler.java
+++ src/main/java/tokyo/nakanaka/roseCurve/commandHandler/CreateCommandHandler.java
@@ -7,6 +7,7 @@ import tokyo.nakanaka.CommandHandler;
 import tokyo.nakanaka.commandSender.CommandSender;
 import tokyo.nakanaka.logger.LogColor;
 import tokyo.nakanaka.roseCurve.Task;
+import tokyo.nakanaka.roseCurve.commandHelp.RcpCommandHelps;
 
 public class CreateCommandHandler implements CommandHandler {
 	private Map<String, Task> taskMap;
@@ -17,23 +18,28 @@ public class CreateCommandHandler implements CommandHandler {
 
 	@Override
 	public void onCommand(CommandSender cmdSender, String[] args) {
-		String usageMsg = LogColor.RED + "Usage: rcp create <taskName>";
+		String usageMsg = LogColor.RED + "Usage: " + RcpCommandHelps.CREATE_HELP;
 		Task task = null;
 		if(args.length == 0) {
 			cmdSender.print(usageMsg);
 			return;
-		}else if(args.length == 1) {
-			task = new Task();
-		}else if(args.length >= 2) {
-			cmdSender.print(usageMsg);
-			return;
 		}
+		//check the same name task
 		if(this.taskMap.get(args[0]) != null) {
 			cmdSender.print(LogColor.RED + "Task \"" + args[0] + "\" already exists! " 
 				+ "For deleting a task, use \"/rcp del\" command.");
 			return;
 		}
+		//try to create new task
+		if(args.length == 1) {
+			task = new Task();
+		}else if(args.length >= 2) {
+			cmdSender.print(usageMsg);
+			return;
+		}
+		//register task
 		this.taskMap.put(args[0], task);
+		cmdSender.print(LogColor.LIGHT_PURPLE + "Create new task \"" + args[0] + "\"");
 	}
 
 	@Override
