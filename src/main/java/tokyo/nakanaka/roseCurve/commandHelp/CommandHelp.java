1a6cc52c4b57837a04e35cc7684b05bedea6c881
nakanakagames <nakanakagames@gmail.com>
2021-08-04 17:25:59 +0900

    write javadoc comment

--- src/main/java/tokyo/nakanaka/roseCurve/commandHelp/CommandHelp.java
+++ src/main/java/tokyo/nakanaka/roseCurve/commandHelp/CommandHelp.java
@@ -3,11 +3,21 @@ package tokyo.nakanaka.roseCurve.commandHelp;
 import java.util.List;
 
 public interface CommandHelp {
-	
+	/**
+	 * @return a usage, which is a simple plain text(It does not contain MOTD code)
+	 */
 	String getUsage();
-	
+	/**
+	 * Return a single line which contains the information for the command.
+	 * This is used by HelpCommandHandler class
+	 * @return a single line which contains the information for the command
+	 */
 	String toSingleLine();
-	
+	/**
+	 * Return multiple lines which contains the information for the command
+	 * This is used by HelpCommandHandler class
+	 * @return multiple lines which contains the information for the command
+	 */
 	List<String> toMultipleLines();
 	
 }
