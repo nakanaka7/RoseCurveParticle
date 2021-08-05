3551628b6dfbcd8e9e2785abce176f888b9180ce
nakanakagames <nakanakagames@gmail.com>
2021-08-06 07:40:19 +0900

    fix <taskName> description/ add "a", "n", "d" subcommand descriptions

--- src/main/java/tokyo/nakanaka/roseCurveParticle/commandHelp/SettingCommandHelp.java
+++ src/main/java/tokyo/nakanaka/roseCurveParticle/commandHelp/SettingCommandHelp.java
@@ -25,8 +25,15 @@ class SettingCommandHelp implements CommandHelp {
 		line.add(LogColor.LIGHT_PURPLE + "Description: " + LogColor.RESET + "Change a setting of the task");
 		line.add(LogColor.LIGHT_PURPLE + "Usage: " + LogColor.RESET + "/rcp setting " + LogColor.LIGHT_PURPLE + "<taskName> [subcommand]");
 		line.add(LogColor.LIGHT_PURPLE + "Parameters: ");
-		line.add("  " + LogColor.LIGHT_PURPLE + "<taskName>: " + LogColor.RESET + "task name of new task");
+		line.add("  " + LogColor.LIGHT_PURPLE + "<taskName>: " + LogColor.RESET + "task name");
 		line.add("  " + LogColor.LIGHT_PURPLE + "[subcommand]: " + LogColor.RESET + "");
+		line.add("    " + LogColor.LIGHT_PURPLE + "<empty>: " + LogColor.RESET + "Nothing changes, just print the current settings");
+		line.add("    " + LogColor.LIGHT_PURPLE + "<a> <double>: " + LogColor.RESET + "Change the " 
+			+ LogColor.LIGHT_PURPLE + "\"a\" " + LogColor.RESET + "factor of the rose curve");
+		line.add("    " + LogColor.LIGHT_PURPLE + "<n> <positive int>: " + LogColor.RESET + "Change the " 
+			+ LogColor.LIGHT_PURPLE + "\"n\" " + LogColor.RESET + "factor of the rose curve");
+		line.add("    " + LogColor.LIGHT_PURPLE + "<d> <positive int>: " + LogColor.RESET + "Change the "
+			+ LogColor.LIGHT_PURPLE + "\"d\" " + LogColor.RESET + "factor of the rose curve");
 		return line;
 	}
 
