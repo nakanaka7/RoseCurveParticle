79b5c9d9ffe3eba628c5ba4a3029111b7f0673a9
nakanakagames <nakanakagames@gmail.com>
2021-08-05 17:19:42 +0900

    javadoc/ add getA/N/DFactor method

--- src/main/java/tokyo/nakanaka/roseCurveParticle/math/RoseCurve.java
+++ src/main/java/tokyo/nakanaka/roseCurveParticle/math/RoseCurve.java
@@ -1,16 +1,43 @@
 package tokyo.nakanaka.roseCurveParticle.math;
-
+/**
+ * Represents a rose curve, r = a * sin((n/d) * θ)
+ */
 public class RoseCurve {
 	private double a;
 	private int n;
 	private int d;
-	
+	/**
+	 * Construct a rose curve, r = a * sin((n/d) * θ)
+	 * @param a a factor
+	 * @param n n factor
+	 * @param d d factor
+	 */
 	public RoseCurve(double a, int n, int d) {
 		this.a = a;
 		this.n = n;
 		this.d = d;
 	}
-	
+	/**
+	 * Get A factor of the curve
+	 * @return A factor of the curve
+	 */
+	public double getAFactor() {
+		return a;
+	}
+	/**
+	 * Get N factor of the curve
+	 * @return N factor of the curve
+	 */
+	public int getNFactor() {
+		return n;
+	}
+	/**
+	 * Get D factor of the curve
+	 * @return D factor of the curve
+	 */
+	public int getDFactor() {
+		return d;
+	}
 	/**
 	 * @return a * sin((n/d) * θ)
 	 */
