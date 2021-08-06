a2955a5bd455fedf8ec5b42ea7920e11d3cec190
nakanakagames <nakanakagames@gmail.com>
2021-08-06 21:28:36 +0900

    add printing the "k"

--- src/main/java/tokyo/nakanaka/roseCurveParticle/commandHandler/CommandHandlerFunctions.java
+++ src/main/java/tokyo/nakanaka/roseCurveParticle/commandHandler/CommandHandlerFunctions.java
@@ -21,9 +21,11 @@ public class CommandHandlerFunctions {
 		List<String> lines = new ArrayList<>();
 		lines.add("--- [" + LogColor.LIGHT_PURPLE + "Settings of " + LogColor.RESET + taskName + "] ---------------------");
 		lines.add(LogColor.LIGHT_PURPLE + "  eq. " + LogColor.RESET + "r = a * sin((n/d) * θ)");
+		lines.add(  "θ = k * t");
 		lines.add(LogColor.LIGHT_PURPLE + "a: " + LogColor.RESET + task.getAFactor());
 		lines.add(LogColor.LIGHT_PURPLE + "n: " + LogColor.RESET + task.getNFactor());
 		lines.add(LogColor.LIGHT_PURPLE + "d: " + LogColor.RESET + task.getDFactor());
+		lines.add(LogColor.LIGHT_PURPLE + "k: " + LogColor.RESET + task.getAngularVelocity() + "[degree/ tick]");
 		lines.add("---------------------------------------");
 		lines.add(LogColor.LIGHT_PURPLE + "World: " + LogColor.RESET + task.getWorld());
 		lines.add(LogColor.LIGHT_PURPLE + "Center: " + LogColor.RESET + task.getCenter());
