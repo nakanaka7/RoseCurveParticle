57c2aa89dcd9fd39e71d4e248a035ecfde69b808
nakanakagames <nakanakagames@gmail.com>
2021-08-06 10:15:19 +0900

    first commit

---
+++ src/main/java/tokyo/nakanaka/roseCurveParticle/commandHandler/settingSub/SettingSubCommandHandler.java
@@ -0,0 +1,25 @@
+package tokyo.nakanaka.roseCurveParticle.commandHandler.settingSub;
+
+import java.util.List;
+
+import tokyo.nakanaka.commandSender.CommandSender;
+import tokyo.nakanaka.roseCurveParticle.Task;
+/**
+ * Handles sub command of setting command. Used in SettingCommandHandler class
+ */
+public interface SettingSubCommandHandler {
+	/**
+	 * Invoked when the sub command is run
+	 * @param cmdSender a command sender who run the command
+	 * @param args arguments of the sub command
+	 */
+	void onCommand(CommandSender cmdSender, String[] args, Task task);
+	/**
+	 * Returns a String list which will be used on auto tab complete for the sub command
+	 * @param cmdSender a command sender who run the command
+	 * @param args arguments of the sub command
+	 * @return a String list for the sub command
+	 */
+	List<String> onTabComplete(CommandSender cmdSender, String[] args);
+			
+}
