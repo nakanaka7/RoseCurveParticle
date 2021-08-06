1f8d7be0a01da9a07f90625e39e4f7ce4f0a0c81
nakanakagames <nakanakagames@gmail.com>
2021-08-06 14:51:02 +0900

    first commit

---
+++ src/main/java/tokyo/nakanaka/roseCurveParticle/commandHandler/settingSub/CenterCommandHandler.java
@@ -0,0 +1,25 @@
+package tokyo.nakanaka.roseCurveParticle.commandHandler.settingSub;
+
+import java.util.List;
+
+import tokyo.nakanaka.commandSender.CommandSender;
+import tokyo.nakanaka.roseCurveParticle.Task;
+
+/**
+ * Handles "center" sub command of "/rcp setting &lt;taskName&gt;" command
+ */
+public class CenterCommandHandler implements SettingSubCommandHandler {
+
+	@Override
+	public void onCommand(CommandSender cmdSender, String[] args, String taskName, Task task) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<String> onTabComplete(CommandSender cmdSender, String[] args) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
