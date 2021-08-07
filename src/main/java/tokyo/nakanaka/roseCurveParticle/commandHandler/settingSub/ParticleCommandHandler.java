d632fa8765556337a1125e46d18081abdddda770
nakanakagames <nakanakagames@gmail.com>
2021-08-07 10:40:11 +0900

    implemented onCommand and onTabComplete method

--- src/main/java/tokyo/nakanaka/roseCurveParticle/commandHandler/settingSub/ParticleCommandHandler.java
+++ src/main/java/tokyo/nakanaka/roseCurveParticle/commandHandler/settingSub/ParticleCommandHandler.java
@@ -3,20 +3,37 @@ package tokyo.nakanaka.roseCurveParticle.commandHandler.settingSub;
 import java.util.List;
 
 import tokyo.nakanaka.commandSender.CommandSender;
+import tokyo.nakanaka.logger.LogColor;
+import tokyo.nakanaka.particle.Particle;
+import tokyo.nakanaka.particle.ParticleParser;
 import tokyo.nakanaka.roseCurveParticle.Task;
+import tokyo.nakanaka.roseCurveParticle.commandHandler.CommandHandlerFunctions;
 
 public class ParticleCommandHandler implements SettingSubCommandHandler {
+	private ParticleParser particleParser;
+	
+	public ParticleCommandHandler(ParticleParser particleParser) {
+		this.particleParser = particleParser;
+	}
 
 	@Override
 	public void onCommand(CommandSender cmdSender, String[] args, String taskName, Task task) {
-		// TODO Auto-generated method stub
-		
+		if(args.length == 0) {
+			cmdSender.print(LogColor.RED + "Usage: /rcp setting <taskName> particle <id> [extra]...");
+			return;
+		}
+		Particle particle = this.particleParser.parse(args);
+		if(particle == null) {
+			cmdSender.print(LogColor.RED + "Can not convert \"" + args[0] + "\" to particle");
+		}
+		task.setParticle(particle);
+		CommandHandlerFunctions.createSettingLines(taskName, task).stream()
+			.forEach(s -> cmdSender.print(s));
 	}
 
 	@Override
 	public List<String> onTabComplete(CommandSender cmdSender, String[] args) {
-		// TODO Auto-generated method stub
-		return null;
+		return this.particleParser.onTabComplete(args);
 	}
 
 }
