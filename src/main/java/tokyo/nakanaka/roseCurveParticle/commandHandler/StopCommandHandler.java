00176933b012b713f6067e5365aec2a3b704b475
nakanakagames <nakanakagames@gmail.com>
2021-08-07 14:22:21 +0900

    first commit

---
+++ src/main/java/tokyo/nakanaka/roseCurveParticle/commandHandler/StopCommandHandler.java
@@ -0,0 +1,46 @@
+package tokyo.nakanaka.roseCurveParticle.commandHandler;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import tokyo.nakanaka.CommandHandler;
+import tokyo.nakanaka.commandSender.CommandSender;
+import tokyo.nakanaka.logger.LogColor;
+import tokyo.nakanaka.roseCurveParticle.Task;
+import tokyo.nakanaka.roseCurveParticle.commandHelp.RcpCommandHelps;
+
+/**
+ * Handles "/rcp stop" command
+ */
+public class StopCommandHandler implements CommandHandler {
+	private Map<String, Task> taskMap;
+	/**
+	 * @param taskMap a map which stores tasks
+	 */
+	public StopCommandHandler(Map<String, Task> taskMap) {
+		this.taskMap = taskMap;
+	}
+	
+	@Override
+	public void onCommand(CommandSender cmdSender, String[] args) {
+		if(args.length != 1) {
+			cmdSender.print(LogColor.RED + "Usage: " + RcpCommandHelps.START_HELP.getUsage());
+		}
+		Task task = this.taskMap.get(args[0]);
+		if(task == null) {
+			cmdSender.print(LogColor.RED + "No task which name is \"" + args[0] + "\"");
+			return;
+		}
+		task.stop();
+		cmdSender.print(LogColor.LIGHT_PURPLE + "Stopped the task " + "\"" + args[0] + "\"");		
+	}
+
+	@Override
+	public List<String> onTabComplete(CommandSender cmdSender, String[] args) {
+		return this.taskMap.entrySet().stream()
+			.map(s -> s.getKey())
+			.collect(Collectors.toList());
+	}
+
+}
