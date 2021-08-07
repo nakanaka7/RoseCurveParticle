ecb3ab9a56d5894f292336eea78cb14ef27396bd
nakanakagames <nakanakagames@gmail.com>
2021-08-07 13:53:44 +0900

    first commit

---
+++ src/main/java/tokyo/nakanaka/roseCurveParticle/commandHelp/StartCommandHelp.java
@@ -0,0 +1,34 @@
+package tokyo.nakanaka.roseCurveParticle.commandHelp;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import tokyo.nakanaka.logger.LogColor;
+
+/** 
+ * CommandHelp for "/rcp start" command
+ */
+public class StartCommandHelp implements CommandHelp {
+
+	@Override
+	public String getUsage() {
+		return "/rcp start <taskName>";
+	}
+
+	@Override
+	public String toSingleLine() {
+		return LogColor.LIGHT_PURPLE + "/rcp start <taskName>" + ": " + LogColor.RESET + "Start or resume a task";
+	}
+
+	@Override
+	public List<String> toMultipleLines() {
+		List<String> line = new ArrayList<>();
+		line.add("--- [" + LogColor.LIGHT_PURPLE + "Help for " + LogColor.RESET + "/rcp start] ---------------------");
+		line.add(LogColor.LIGHT_PURPLE + "Description: " + LogColor.RESET + "Start or resume a task");
+		line.add(LogColor.LIGHT_PURPLE + "Usage: " + LogColor.RESET + "/rcp start " + LogColor.LIGHT_PURPLE + "<taskName>");
+		line.add(LogColor.LIGHT_PURPLE + "Parameters: ");
+		line.add("  " + LogColor.LIGHT_PURPLE + "<taskName>: " + LogColor.RESET + "a task name to start");
+		return line;
+	}
+
+}
