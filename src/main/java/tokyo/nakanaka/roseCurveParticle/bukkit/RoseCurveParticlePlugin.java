939caa4ff9fcfe1039a7c29154edb2b8e3ec4fb9
nakanakagames <nakanakagames@gmail.com>
2021-08-12 23:21:54 +0900

    fix to match new main constructor

--- src/main/java/tokyo/nakanaka/roseCurveParticle/bukkit/RoseCurveParticlePlugin.java
+++ src/main/java/tokyo/nakanaka/roseCurveParticle/bukkit/RoseCurveParticlePlugin.java
@@ -1,21 +1,22 @@
 package tokyo.nakanaka.roseCurveParticle.bukkit;
 
+import java.util.List;
+
 import org.bukkit.command.Command;
 import org.bukkit.plugin.java.JavaPlugin;
+
 import tokyo.nakanaka.bukkit.BukkitFunctions;
-import tokyo.nakanaka.bukkit.BukkitParticleParser;
 import tokyo.nakanaka.bukkit.BukkitScheduler;
 import tokyo.nakanaka.bukkit.BukkitWorldFinder;
+import tokyo.nakanaka.bukkit.particle.BukkitParticleParseHandler;
 import tokyo.nakanaka.commandSender.CommandSender;
 import tokyo.nakanaka.roseCurveParticle.Main;
-
-import java.util.List;
 
 /**
  * An entry point of this plugin
  */
 public class RoseCurveParticlePlugin extends JavaPlugin {
-	private Main main = new Main(new BukkitScheduler(this), new BukkitParticleParser(), new BukkitWorldFinder(this.getServer()));
+	private Main main = new Main(new BukkitScheduler(this), new BukkitParticleParseHandler(), new BukkitWorldFinder(this.getServer()));
 	
 	@Override
 	public boolean onCommand(org.bukkit.command.CommandSender cmdSender0, Command command, String label, String[] args) {
