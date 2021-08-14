766879e80e4807262c5e22882a3b7d627f77a0d6
nakanakagames <nakanakagames@gmail.com>
2021-08-14 21:26:24 +0900

    first commit

---
+++ src/main/java/tokyo/nakanaka/roseCurveParticle/bukkit/BukkitParticleParseHandler.java
@@ -0,0 +1,22 @@
+package tokyo.nakanaka.roseCurveParticle.bukkit;
+
+import java.util.List;
+
+import tokyo.nakanaka.particle.Particle;
+import tokyo.nakanaka.roseCurveParticle.ParticleParseHandler;
+
+public class BukkitParticleParseHandler implements ParticleParseHandler {
+
+	@Override
+	public Particle onParse(String label, String[] args) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<String> onTabComplete(String label, String[] args) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
