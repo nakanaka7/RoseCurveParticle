70c24da6055423fc1c89b78863960602bc73dc5a
nakanakagames <nakanakagames@gmail.com>
2021-08-14 22:58:04 +0900

    first commit

---
+++ src/main/java/tokyo/nakanaka/roseCurveParticle/bukkit/particleParseHandler/BlockParticleParseHandler.java
@@ -0,0 +1,44 @@
+package tokyo.nakanaka.roseCurveParticle.bukkit.particleParseHandler;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.bukkit.Material;
+
+import tokyo.nakanaka.block.Block;
+import tokyo.nakanaka.particle.BlockParticle;
+import tokyo.nakanaka.particle.Particle;
+import tokyo.nakanaka.particle.BlockParticle.Type;
+import tokyo.nakanaka.roseCurveParticle.SubParticleParseHandler;
+
+public class BlockParticleParseHandler implements SubParticleParseHandler {
+
+	@Override
+	public Particle onParse(String[] args) {
+		if(args.length != 2) {
+			throw new IllegalArgumentException();
+		}
+		Block block = Block.valueOf(args[0]);
+		Type type = Type.valueOf(args[1]);
+		return new BlockParticle(block, type);
+	}
+
+	@Override
+	public List<String> onTabComplete(String[] args) {
+		if(args.length == 0) {
+			return List.of();
+		}else if(args.length == 1) {
+			return List.of(Material.values()).stream()
+				.filter(s -> s.isBlock())
+				.map(s -> "minecraft:" + s.toString().toLowerCase())
+				.collect(Collectors.toList());
+		}else if(args.length == 2) {
+			return List.of(BlockParticle.Type.values()).stream()
+					.map(s-> s.toString().toLowerCase())
+					.collect(Collectors.toList());
+		}else {
+			return List.of();
+		}
+	}
+	
+}
---
+++ src/main/java/tokyo/nakanaka/roseCurveParticle/bukkit/particleParseHandler/DustParticleHandler.java
@@ -0,0 +1,34 @@
+package tokyo.nakanaka.roseCurveParticle.bukkit.particleParseHandler;
+
+import java.util.List;
+
+import tokyo.nakanaka.particle.DustParticle;
+import tokyo.nakanaka.roseCurveParticle.SubParticleParseHandler;
+
+public class DustParticleHandler implements SubParticleParseHandler {
+	
+	@Override
+	public DustParticle onParse(String[] args) {
+		if(args.length != 4) {
+			throw new IllegalArgumentException();
+		}
+		float red = Float.parseFloat(args[0]);
+		float green = Float.parseFloat(args[1]);
+		float blue = Float.parseFloat(args[2]);
+		float size = Float.parseFloat(args[3]);
+		return new DustParticle(red, green, blue, size);
+	}
+	
+	@Override
+	public List<String> onTabComplete(String[] args) {
+		if(1 <= args.length && args.length <= 3) {
+			return List.of("0", "0.1", "0.2", "0.3", "0.4", "0.5", "0.6", "0.7", "0.8", "0.9", "1");
+		}else if(args.length == 4) {
+			return List.of("0.5", "1.0", "1.5", "2.0", "2.5", "3.0", "3.5", "4.0", "4.5", "5.0"
+					, "5.5", "6.0", "6.5", "7.0", "7.5", "8.0", "8.5", "9.0", "9.5");
+		}else {
+			return List.of();
+		}
+	}
+	
+}
