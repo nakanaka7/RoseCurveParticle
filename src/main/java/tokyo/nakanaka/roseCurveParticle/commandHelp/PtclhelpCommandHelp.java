b84c54472b16b37a9cbaa1ae6357bb72f228edfd
nakanakagames <nakanakagames@gmail.com>
2021-08-17 22:33:49 +0900

    first commit

---
+++ src/main/java/tokyo/nakanaka/roseCurveParticle/commandHelp/PtclhelpCommandHelp.java
@@ -0,0 +1,31 @@
+package tokyo.nakanaka.roseCurveParticle.commandHelp;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import tokyo.nakanaka.logger.LogColor;
+
+public class PtclhelpCommandHelp implements CommandHelp {
+
+	@Override
+	public String getUsage() {
+		return "/rcp ptclhelp [particle]";
+	}
+
+	@Override
+	public String toSingleLine() {
+		return LogColor.LIGHT_PURPLE + "/rcp ptclhelp [particle]" + ": " + LogColor.RESET + "Print the help for special particle(s)";
+	}
+
+	@Override
+	public List<String> toMultipleLines() {
+		List<String> line = new ArrayList<>();
+		line.add("--- [" + LogColor.LIGHT_PURPLE + "Help for " + LogColor.RESET + "/rcp ptclhelp] ---------------------");
+		line.add(LogColor.LIGHT_PURPLE + "Description: " + LogColor.RESET + "Print the help for special particle(s)");
+		line.add(LogColor.LIGHT_PURPLE + "Usage: " + LogColor.RESET + "/rcp ptclhelp " + LogColor.LIGHT_PURPLE + "[particle]");
+		line.add(LogColor.LIGHT_PURPLE + "Parameters: ");
+		line.add("  " + LogColor.LIGHT_PURPLE + "[particle]: " + LogColor.RESET + "particle to print the help");
+		return line;
+	}
+
+}
