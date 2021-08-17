4d780450441883370b536c806edc8615cb26eaa0
nakanakagames <nakanakagames@gmail.com>
2021-08-17 17:07:45 +0900

    refactor method names

--- src/main/java/tokyo/nakanaka/roseCurveParticle/commandHandler/particleHelp/ParticleHelp.java
+++ src/main/java/tokyo/nakanaka/roseCurveParticle/commandHandler/particleHelp/ParticleHelp.java
@@ -1,19 +1,19 @@
-package tokyo.nakanaka.roseCurveParticle.commandHandler.settingSub.particleHelpCreator;
+package tokyo.nakanaka.roseCurveParticle.commandHandler.particleHelp;
 
 import java.util.List;
 
 /**
  * Create help lines for a particle
  */
-public interface ParticleHelpCreator {
+public interface ParticleHelp {
     /**
      * Create single line help for the particle
      * @return single line
      */
-    public String createSingleLine();
+    public String toSingleLine();
     /**
      * Create multiple lines help for the particle
      * @return multiple lines
      */
-    public List<String> createMultipleLines();
+    public List<String> toMultipleLines();
 }
