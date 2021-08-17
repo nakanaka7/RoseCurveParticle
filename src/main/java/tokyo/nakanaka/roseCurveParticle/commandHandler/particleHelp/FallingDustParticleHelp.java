687356e6f2c8f4730090f5e328d142c4e77b3f7e
nakanakagames <nakanakagames@gmail.com>
2021-08-17 22:47:48 +0900

    first commit

---
+++ src/main/java/tokyo/nakanaka/roseCurveParticle/commandHandler/particleHelp/FallingDustParticleHelp.java
@@ -0,0 +1,25 @@
+package tokyo.nakanaka.roseCurveParticle.commandHandler.particleHelp;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import tokyo.nakanaka.logger.LogColor;
+
+public class FallingDustParticleHelp implements ParticleHelp {
+
+	@Override
+	public String toSingleLine() {
+		return "minecraft:falling_dust <block>";
+	}
+
+	@Override
+	public List<String> toMultipleLines() {
+		List<String> line = new ArrayList<>();
+		line.add("--- [" + LogColor.LIGHT_PURPLE + "Help for " + LogColor.RESET + "\"minecraft:falling_dust\" particle] ---------------------");
+		line.add(LogColor.LIGHT_PURPLE + "Usage: " + LogColor.RESET + "\"minecraft:falling_dust\" " + LogColor.LIGHT_PURPLE + "<block>");
+		line.add(LogColor.LIGHT_PURPLE + "Parameters: ");
+		line.add("  " + LogColor.LIGHT_PURPLE + "<block>: " + LogColor.RESET + "block");
+		return line;
+	}
+
+}
