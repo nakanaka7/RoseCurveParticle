7d0d1827f6711ae7cb0389ea9e1302a61786f448
nakanakagames <nakanakagames@gmail.com>
2021-08-17 22:11:27 +0900

    first commit

---
+++ src/main/java/tokyo/nakanaka/roseCurveParticle/commandHandler/particleHelp/BlockParticleHelp.java
@@ -0,0 +1,26 @@
+package tokyo.nakanaka.roseCurveParticle.commandHandler.particleHelp;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import tokyo.nakanaka.logger.LogColor;
+
+public class BlockParticleHelp implements ParticleHelp {
+
+	@Override
+	public String toSingleLine() {
+		return "minecraft:block <block> [crack|dust]";
+	}
+
+	@Override
+	public List<String> toMultipleLines() {
+		List<String> line = new ArrayList<>();
+		line.add("--- [" + LogColor.LIGHT_PURPLE + "Help for " + LogColor.RESET + "\"minecraft:block\"] ---------------------");
+		line.add(LogColor.LIGHT_PURPLE + "Usage: " + LogColor.RESET + "\"minecraft:block\" " + LogColor.LIGHT_PURPLE + "<block> [crack|dust]");
+		line.add(LogColor.LIGHT_PURPLE + "Parameters: ");
+		line.add("  " + LogColor.LIGHT_PURPLE + "<block>: " + LogColor.RESET + "block");
+		line.add("  " + LogColor.LIGHT_PURPLE + "[crack|dust]: " + LogColor.RESET + "crack or dust option, default is \"dust\"");
+		return line;
+	}
+
+}
