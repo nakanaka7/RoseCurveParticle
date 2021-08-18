fded8cb0ac4289d5377c9c6862a915ba6ecf23bd
nakanakagames <nakanakagames@gmail.com>
2021-08-18 10:16:37 +0900

    delete

--- src/main/java/tokyo/nakanaka/roseCurveParticle/MainDependency.java
+++
@@ -1,8 +0,0 @@
-package tokyo.nakanaka.roseCurveParticle;
-
-import tokyo.nakanaka.Scheduler;
-import tokyo.nakanaka.WorldFinder;
-
-public record MainDependency(Scheduler scheduler, ParticleParseHandler particleParseHandler, WorldFinder worldFinder) {
-
-}
