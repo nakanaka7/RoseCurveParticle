b4e34cb4e265637ba8e3dac22e366422e2193e49
nakanakagames <nakanakagames@gmail.com>
2021-08-18 14:27:00 +0900

    add requires

--- src/module-info.java
+++ src/module-info.java
@@ -1,4 +1,6 @@
 module tokyo.nakanaka.roseCurveParticle {
+	requires transitive NakanakaCommons;
+	requires transitive org.bukkit;
 	exports tokyo.nakanaka.roseCurveParticle;
 	exports tokyo.nakanaka.roseCurveParticle.bukkit;
 }
