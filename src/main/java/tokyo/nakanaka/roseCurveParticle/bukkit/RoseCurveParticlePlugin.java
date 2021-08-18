20937d65d0e7d7e311ec443cd40f4ad60e06ac87
nakanakagames <nakanakagames@gmail.com>
2021-08-18 09:31:20 +0900

    use MainDependency to construct Main

--- src/main/java/tokyo/nakanaka/roseCurveParticle/bukkit/RoseCurveParticlePlugin.java
+++ src/main/java/tokyo/nakanaka/roseCurveParticle/bukkit/RoseCurveParticlePlugin.java
@@ -5,18 +5,31 @@ import java.util.List;
 import org.bukkit.command.Command;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import tokyo.nakanaka.Scheduler;
+import tokyo.nakanaka.WorldFinder;
 import tokyo.nakanaka.bukkit.BukkitFunctions;
 import tokyo.nakanaka.bukkit.BukkitScheduler;
 import tokyo.nakanaka.bukkit.BukkitWorldFinder;
 import tokyo.nakanaka.commandSender.CommandSender;
 import tokyo.nakanaka.roseCurveParticle.Main;
+import tokyo.nakanaka.roseCurveParticle.MainDependency;
+import tokyo.nakanaka.roseCurveParticle.ParticleParseHandler;
 import tokyo.nakanaka.roseCurveParticle.bukkit.particleParseHandler.BukkitParticleParseHandler;
 
 /**
  * An entry point of this plugin
  */
 public class RoseCurveParticlePlugin extends JavaPlugin {
-	private Main main = new Main(new BukkitScheduler(this), new BukkitParticleParseHandler(), new BukkitWorldFinder(this.getServer()));
+	private Main main;
+	
+	@Override
+	public void onLoad() {
+		Scheduler scheduler = new BukkitScheduler(this);
+		ParticleParseHandler ptclParseHandler = new BukkitParticleParseHandler();
+		WorldFinder worldFinder = new BukkitWorldFinder(this.getServer());
+		MainDependency mainDependency = new MainDependency(scheduler, ptclParseHandler, worldFinder);
+		this.main = new Main(mainDependency);
+	}
 	
 	@Override
 	public boolean onCommand(org.bukkit.command.CommandSender cmdSender0, Command command, String label, String[] args) {
