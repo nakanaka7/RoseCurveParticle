4176cba45d97517b81dd4e51fa56d26acd5223b0
nakanakagames <nakanakagames@gmail.com>
2021-08-18 11:02:29 +0900

    remove crack|dust option

--- src/main/java/tokyo/nakanaka/roseCurveParticle/commandHandler/particleHelp/BlockParticleHelp.java
+++ src/main/java/tokyo/nakanaka/roseCurveParticle/commandHandler/particleHelp/BlockParticleHelp.java
@@ -9,17 +9,16 @@ public class BlockParticleHelp implements ParticleHelp {
 
 	@Override
 	public String toSingleLine() {
-		return "minecraft:block " + LogColor.LIGHT_PURPLE + "<block> [crack|dust]";
+		return "minecraft:block " + LogColor.LIGHT_PURPLE + "<block>";
 	}
 
 	@Override
 	public List<String> toMultipleLines() {
 		List<String> line = new ArrayList<>();
 		line.add("--- [" + LogColor.LIGHT_PURPLE + "Help for " + LogColor.RESET + "\"minecraft:block\" particle] ---------------------");
-		line.add(LogColor.LIGHT_PURPLE + "Usage: " + LogColor.RESET + "\"minecraft:block\" " + LogColor.LIGHT_PURPLE + "<block> [crack|dust]");
+		line.add(LogColor.LIGHT_PURPLE + "Usage: " + LogColor.RESET + "\"minecraft:block\" " + LogColor.LIGHT_PURPLE + "<block>");
 		line.add(LogColor.LIGHT_PURPLE + "Parameters: ");
 		line.add("  " + LogColor.LIGHT_PURPLE + "<block>: " + LogColor.RESET + "block");
-		line.add("  " + LogColor.LIGHT_PURPLE + "[crack|dust]: " + LogColor.RESET + "crack or dust option, default is \"dust\"");
 		return line;
 	}
 
